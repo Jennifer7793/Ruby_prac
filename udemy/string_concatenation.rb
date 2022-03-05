7188940cfea7f82cf02a76170739b51b981e03e8
JenJen <jennifer7793@gmail.com>
2022-03-05 14:21:48 +0800

    use sub method to change string content

--- udemy/string_concatenation.rb
+++ udemy/string_concatenation.rb
@@ -5,3 +5,7 @@ p first_name + " " + last_name
 p "My first name is #{first_name} and my last name is #{last_name}"
 p 'My first name is #{first_name} and my last name is #{last_name}'
 # "My first name is \#{first_name} and my last name is \#{last_name}"
+
+sentence = "Welcome to the jungle"
+p sentence
+p sentence.sub("the jungle", "utopia") # **sub = substitute, gsub = global substitute
