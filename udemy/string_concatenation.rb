9ebfc58abe8da3d944768fa3f28f7ee50a0f2f4a
JenJen <jennifer7793@gmail.com>
2022-03-05 14:21:17 +0800

    string_concatentation

---
+++ udemy/string_concatenation.rb
@@ -0,0 +1,7 @@
+first_name = "Jennifer"
+last_name = "Lin"
+p first_name + last_name
+p first_name + " " + last_name
+p "My first name is #{first_name} and my last name is #{last_name}"
+p 'My first name is #{first_name} and my last name is #{last_name}'
+# "My first name is \#{first_name} and my last name is \#{last_name}"
