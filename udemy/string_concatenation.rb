c44475a94f37d90f9addc6793d468a5d5ddd7596
JenJen <jennifer7793@gmail.com>
2022-03-05 14:35:57 +0800

    Variable assignement: a variable is pointing to another variable, is pointing to location memory where variable points

--- udemy/string_concatenation.rb
+++ udemy/string_concatenation.rb
@@ -9,3 +9,9 @@ p 'My first name is #{first_name} and my last name is #{last_name}'
 sentence = "Welcome to the jungle"
 p sentence
 p sentence.sub("the jungle", "utopia") # **sub = substitute, gsub = global substitute
+
+new_first_name = first_name
+p new_first_name
+first_name = "Yi Chen"
+p first_name
+p new_first_name
