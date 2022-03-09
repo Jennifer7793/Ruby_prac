f372e4a414ae1bc1a383ae2c61e2545c80dbfc3e
JenJen <jennifer7793@gmail.com>
2022-03-09 20:51:03 +0800

    codewar string double char

---
+++ codewar/doublechar.rb
@@ -0,0 +1,13 @@
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  
+
+def double_char(str)
+  str.chars.map { |s| s + s }.join
+end
+
+p double_char("String")
+p double_char("Hello World")
+p double_char("1234!__")
