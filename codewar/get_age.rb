1b5a3f23e808ee80f3534920a9b84fbb473b1438
JenJen <jennifer7793@gmail.com>
2022-04-03 08:11:26 +0800

    use char and chars

--- codewar/get_age.rb
+++ codewar/get_age.rb
@@ -1,6 +1,13 @@
 def get_age(age)
   # "#{age}".to_i 
-  age.to_i
+  # age.to_i
+end
+
+def get_age(age)
+  age.chr.to_i
+  # age.chr will bcum "5"
+  age.chars[0].to_i
+  # age.chars = ["5", " ", "y", "e", "a", "r", "s", " ", "o", "l", "d"] 
 end
 
 p get_age("2 years old")
