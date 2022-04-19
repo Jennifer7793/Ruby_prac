1c7882ee1f8f9cc76b1efbc023d375f102ff8e46
JenJen <jennifer7793@gmail.com>
2022-04-19 16:53:26 +0800

    mahjong render 4 sets of shoupai, shuffle leftover n give accordingly by index till leftover.length ==14

--- mahjiang_pai2.rb
+++ mahjiang_pai2.rb
@@ -1,6 +1,4 @@
-def random_shoupai
-  pai = ['m1', 'm1', 'm1', 'm1', 'm2', 'm2', 'm2', 'm2', 'm3', 'm3', 'm3', 'm3', 'm4', 'm4', 'm4', 'm4', 'm0', 'm5', 'm5', 'm5', 'm6', 'm6', 'm6', 'm6', 'm7', 'm7', 'm7', 'm7', 'm8', 'm8', 'm8', 'm8', 'm9', 'm9', 'm9', 'm9', 'p1', 'p1', 'p1', 'p1', 'p2', 'p2', 'p2', 'p2', 'p3', 'p3', 'p3', 'p3', 'p4', 'p4', 'p4', 'p4', 'p0', 'p5', 'p5', 'p5', 'p6', 'p6', 'p6', 'p6', 'p7', 'p7', 'p7', 'p7', 'p8', 'p8', 'p8', 'p8', 'p9', 'p9', 'p9', 'p9', 's1', 's1', 's1', 's1', 's2', 's2', 's2', 's2', 's3', 's3', 's3', 's3', 's4', 's4', 's4', 's4', 's0', 's5', 's5', 's5', 's6', 's6', 's6', 's6', 's7', 's7', 's7', 's7', 's8', 's8', 's8', 's8', 's9', 's9', 's9', 's9', 'z1', 'z1', 'z1', 'z1', 'z2', 'z2', 'z2', 'z2', 'z3', 'z3', 'z3', 'z3', 'z4', 'z4', 'z4', 'z4', 'z5', 'z5', 'z5', 'z5', 'z6', 'z6', 'z6', 'z6', 'z7', 'z7', 'z7', 'z7']
-
+class Array
   def difference(shoupai)
     h = shoupai.each_with_object(Hash.new(0)) { |e,h| h[e] += 1 }
     # each_with_object 可將array 內的元素分別處理 -> Handy for building up hashes or reducing collections down to one object.
@@ -13,16 +11,29 @@ def random_shoupai
     # 此處reject + block 出去，是組合pai + 上面的h 繼續處理
     # 從pai 裡拿h 內元素不等於0 的牌，然後將其-1，直到變成0
   end
+end
 
-  p1 = pai.sample(13).sort
-  p p1
-  p2 = pai.difference(p1).sample(13).sort
-  p p2
-  p3 = pai.difference(p1+p2).sample(13).sort
-  p p3
-  p4 = pai.difference(p1+p2+p3).sample(13).sort
-  p p4
-  leftover = pai.difference(p1+p2+p3+p4)
+def random_shoupai
+  pai = ['m1', 'm1', 'm1', 'm1', 'm2', 'm2', 'm2', 'm2', 'm3', 'm3', 'm3', 'm3', 'm4', 'm4', 'm4', 'm4', 'm0', 'm5', 'm5', 'm5', 'm6', 'm6', 'm6', 'm6', 'm7', 'm7', 'm7', 'm7', 'm8', 'm8', 'm8', 'm8', 'm9', 'm9', 'm9', 'm9', 'p1', 'p1', 'p1', 'p1', 'p2', 'p2', 'p2', 'p2', 'p3', 'p3', 'p3', 'p3', 'p4', 'p4', 'p4', 'p4', 'p0', 'p5', 'p5', 'p5', 'p6', 'p6', 'p6', 'p6', 'p7', 'p7', 'p7', 'p7', 'p8', 'p8', 'p8', 'p8', 'p9', 'p9', 'p9', 'p9', 's1', 's1', 's1', 's1', 's2', 's2', 's2', 's2', 's3', 's3', 's3', 's3', 's4', 's4', 's4', 's4', 's0', 's5', 's5', 's5', 's6', 's6', 's6', 's6', 's7', 's7', 's7', 's7', 's8', 's8', 's8', 's8', 's9', 's9', 's9', 's9', 'z1', 'z1', 'z1', 'z1', 'z2', 'z2', 'z2', 'z2', 'z3', 'z3', 'z3', 'z3', 'z4', 'z4', 'z4', 'z4', 'z5', 'z5', 'z5', 'z5', 'z6', 'z6', 'z6', 'z6', 'z7', 'z7', 'z7', 'z7']
+
+  pai_1 = pai.sample(13).sort
+  p pai_1
+  pai_2 = pai.difference(pai_1).sample(13).sort
+  p pai_2
+  pai_3 = pai.difference(pai_1+pai_2).sample(13).sort
+  p pai_3
+  pai_4 = pai.difference(pai_1+pai_2+pai_3).sample(13).sort
+  p pai_4
+
+  all_shoupai = pai_1+pai_2+pai_3+pai_4
+  leftover = pai.difference(all_shoupai)
+  p leftover
+  leftover = leftover.shuffle
+  p leftover
+  until leftover.length == 14
+    get_pai = leftover.shift
+    p get_pai
+  end
   p leftover
 end
 
