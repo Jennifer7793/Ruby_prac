1f56ca5e28d0b71240b8ee2c1c77dabef5ddc573
Jenjen <jennifer7793@gmail.com>
2022-05-13 15:26:46 +0800

    if elsif elsif else

--- udemy/branching.rb
+++ udemy/branching.rb
@@ -1,13 +1,24 @@
-#if/else
-
-condition = false
-another_condition = false
-if !condition || !another_condition
-# if !true
-  # execute some code
-  puts "this evaluated to true"
-else
-  # execute some other code
-  puts "this evaluated to false"
+name = "Jennifer"
+if name == "Jennifer"
+  puts "Welcome to the RoR world, Jennifer"
+elsif name == "KL"
+  puts "Welcome to the RoR world, KL"
+elsif name == "Jenjen"
+  puts "Welcome to the RoR world"
+else 
+  puts "Welcome to the RoR world, User"
 end
-puts "La la la"
+
+# #if/else
+
+# condition = false
+# another_condition = false
+# if !condition || !another_condition
+# # if !true
+#   # execute some code
+#   puts "this evaluated to true"
+# else
+#   # execute some other code
+#   puts "this evaluated to false"
+# end
+# puts "La la la"
