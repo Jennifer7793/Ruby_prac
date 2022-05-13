43f53a3018c22fa441701488f5f59605afd5c90f
Jenjen <jennifer7793@gmail.com>
2022-05-13 13:54:46 +0800

    gets chomp

--- udemy/gets_chomp.rb
+++ udemy/gets_chomp.rb
@@ -2,3 +2,7 @@ puts "Enter a number to multiply by 2"
 input = gets.chomp # **when use gets.chomp, will get a string
 puts input * 2 # input: 6 -> "6" + "6" = 66
 p input.to_i * 2 # input 6 -> 6 * 2 = 12
+
+puts "What is your first name?"
+first_name = gets.chomp
+p "Hello, " + first_name 
