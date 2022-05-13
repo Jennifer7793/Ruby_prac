8fd95be46d93245bae615dfc8ebee809468a1812
Jenjen <jennifer7793@gmail.com>
2022-05-13 14:16:56 +0800

    string n num math

---
+++ udemy/19-num.rb
@@ -0,0 +1,13 @@
+puts 1 + 2
+puts 10/4 # will drop the decimal, so return 2
+puts 10.0/4
+puts 10/4.to_f
+puts (10/4).to_f # get 2.0, coz the bracket result is 2 then 2.to_f will be 2.0
+
+x = 5
+y = 10
+puts y/x
+
+puts "I'm a line"
+puts "-"*20
+puts "I'm a diff line after a divider"
