d3e7f45544b61c6b6875fff976ea582af7e93458
Jenjen <jennifer7793@gmail.com>
2022-05-13 14:33:54 +0800

    simple calculator for multiplied

--- udemy/19-num.rb
+++ udemy/19-num.rb
@@ -1,17 +1,26 @@
-puts 1 + 2
-puts 10/4 # will drop the decimal, so return 2
-puts 10.0/4
-puts 10/4.to_f
-puts (10/4).to_f # get 2.0, coz the bracket result is 2 then 2.to_f will be 2.0
+puts "Simple calculator"
+25.times {print "-"}
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the 2nd number"
+num_2 = gets.chomp
+puts "The first number multiplied by the 2nd number is #{num_1.to_i * num_2.to_i}"
 
-x = 5
-y = 10
-puts y/x
 
-puts "I'm a line"
-puts "-"*20
-puts "I'm a diff line after a divider"
+# puts 1 + 2
+# puts 10/4 # will drop the decimal, so return 2
+# puts 10.0/4
+# puts 10/4.to_f
+# puts (10/4).to_f # get 2.0, coz the bracket result is 2 then 2.to_f will be 2.0
 
-20.times { print "-" }
-20.times { puts "hi" }
-20.times { puts rand(10) } #rand(10) random num between 0-9
+# x = 5
+# y = 10
+# puts y/x
+
+# puts "I'm a line"
+# puts "-"*20
+# puts "I'm a diff line after a divider"
+
+# 20.times { print "-" }
+# 20.times { puts "hi" }
+# 20.times { puts rand(10) } #rand(10) random num between 0-9
