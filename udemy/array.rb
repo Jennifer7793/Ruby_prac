d0100ca8938016c347a28dcb2172e364ad372cde
Jenjen <jennifer7793@gmail.com>
2022-05-13 16:45:29 +0800

    arr methods and use %w() transfer to individual string with a ,

--- udemy/array.rb
+++ udemy/array.rb
@@ -2,3 +2,24 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print a # print straight behind 
 puts # new line and will puts individual elements each line
 p a # print arr in the new line
+p a.last
+p a.include?("Jen") #false
+p a.push("new item") #[1, 2, 3, 4, 5, 6, 7, 8, 9, "new item"]
+p b = a.pop #pop for last in first out
+p a #[1, 2, 3, 4, 5, 6, 7, 8, 9]
+p b #"new item"
+
+p %w(my name is Jen and this is great, I'll be master at Ruby!)
+
+x = 1..100
+p x.class #Range
+p x #1..100
+p x.to_a
+p x.to_a.shuffle
+p x #1..100
+p x.to_a.shuffle!
+p x #1..100
+p z = x.to_a.shuffle!
+p z.empty?
+y = []
+p y.empty?
