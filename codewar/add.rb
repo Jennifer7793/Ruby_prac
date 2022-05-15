077bebc30cb26f45e0ba08fb98bbc823f3e7c901
Jenjen <jennifer7793@gmail.com>
2022-05-15 21:39:40 +0800

    use call to return self + the following num

--- codewar/add.rb
+++ codewar/add.rb
@@ -1,6 +1,27 @@
+# def add(n)
+#   n
+# end
+
+# class Fixnum
+#   def call(m=0)
+#     self + m
+#   end
+# end
+
 def add(n)
-  arr = (1..n).to_a.sum
+  def call(m)
+    return self + m
+  end
+  return n
 end
 
-p add(1)
-p add(3)
+p add(1).(2).(3); #6
+p add(1).(2).(3).(4); # 10
+p add(1).(2).(3).(4).(5); # 15
+
+# def add(n)
+#   arr = (1..n).to_a.sum
+# end
+
+# p add(1)
+# p add(3)
