be74f085da17fcb1c0f83260c2f9243e43f585d9
Jenjen <jennifer7793@gmail.com>
2022-06-01 13:50:41 +0800

    use chars and reduce for calculation

--- billboard.rb
+++ billboard.rb
@@ -1,8 +1,11 @@
 def billboard(name, price=30)
   # name.split('').count * price
-  sum = 0
-  name.length.times{ sum += price }
-  sum
+
+  name.chars.reduce(0) { |acc, char| acc + price}
+
+  # sum = 0
+  # name.length.times{ sum += price }
+  # sum
 end
 
 p billboard("Abishai Charalampos")
