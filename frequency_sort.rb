84c9d5baefe221b78c89d1eafe3caa5a53bc1a8f
Jenjen <jennifer7793@gmail.com>
2022-06-17 08:36:22 +0800

    use Hash.new to count each num of qty

--- frequency_sort.rb
+++ frequency_sort.rb
@@ -1,8 +1,12 @@
 def solve(arr)
   # sort_arr = arr.sort
-  # sort_arr.map { |e| sort_arr.count(e) > 1}
-  arr.max_by { |i| arr.count(i) }
+  # sort_arr.map { |e| sort_arr.count(e) }
+  # arr.max_by { |i| arr.count(i) }
+  arr_count = Hash.new(0)
+  arr.each { |arr| arr_count[arr] += 1 }
+  arr_count.sort_by { |arr, num| num }
 end
 
 
 p solve([2,3,5,3,7,9,5,3,7]) # [3,3,3,5,5,7,7,2,9]
+p solve([5,9,6,9,6,5,9,9,4,4]) #[9,9,9,9,4,4,5,5,6,6]
