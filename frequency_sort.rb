f5bd12ab9cb65439d2d64ab2bc197b34bc2aced1
Jenjen <jennifer7793@gmail.com>
2022-06-18 09:22:59 +0800

    sort same numbers tgt by ori frequency

--- frequency_sort.rb
+++ frequency_sort.rb
@@ -1,12 +1,19 @@
 def solve(arr)
   # sort_arr = arr.sort
   # sort_arr.map { |e| sort_arr.count(e) }
-  # arr.max_by { |i| arr.count(i) }
-  arr_count = Hash.new(0)
-  arr.each { |arr| arr_count[arr] += 1 }
-  arr_count.sort_by { |arr, num| num }
-end
 
+  # arr.max_by { |i| arr.count(i) }
+
+  # arr_count = Hash.new(0)
+  # arr.each { |arr| arr_count[arr] += 1 }
+  # arr_count.sort_by { |arr| }
+
+  arr.group_by do |e|
+    e
+  end.map do |k, v|
+    v
+  end
+end
 
 p solve([2,3,5,3,7,9,5,3,7]) # [3,3,3,5,5,7,7,2,9]
 p solve([5,9,6,9,6,5,9,9,4,4]) #[9,9,9,9,4,4,5,5,6,6]
