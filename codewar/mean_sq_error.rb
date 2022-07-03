5f7ee7a048372a904b88cb23fc23b2f0b0e19089
Jenjen <jennifer7793@gmail.com>
2022-07-03 20:39:59 +0800

    mean_sq_error.solution

--- codewar/mean_sq_error.rb
+++ codewar/mean_sq_error.rb
@@ -2,6 +2,10 @@ def solution (arr1, arr2)
   arr1.map.with_index { |n, i| (n - arr2[i]).abs ** 2 }.sum.fdiv(arr1.length)
 end
 
+p solution([1, 2, 3], [4, 5, 6])
+p solution([10, 20, 10, 2], [10, 25, 5, -2])
+p solution([-1, 0], [0, -1])
+
 # fdiv()將兩個數字相除後，結果為浮點數, num1為被除數, num2除數
 num1 = 5
 num2 = 2
