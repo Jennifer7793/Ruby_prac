3be53fa7d6d55fbf9d142cce29518693854f1794
Jenjen <jennifer7793@gmail.com>
2022-07-03 20:47:51 +0800

    .map.with_index

--- codewar/mean_sq_error.rb
+++ codewar/mean_sq_error.rb
@@ -1,4 +1,5 @@
 def solution (arr1, arr2)
+  arr1.map.with_index { |val, index| puts "index: #{index} for #{val}" }
   arr1.map.with_index { |n, i| (n - arr2[i]).abs ** 2 }.sum.fdiv(arr1.length)
 end
 
