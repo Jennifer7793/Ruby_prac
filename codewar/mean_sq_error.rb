19f0a8cfc0e2e103709d58969508f8f961fed479
Jenjen <jennifer7793@gmail.com>
2022-07-03 20:24:52 +0800

    fdiv() to do division and return decimal

--- codewar/mean_sq_error.rb
+++ codewar/mean_sq_error.rb
@@ -1,3 +1,8 @@
 def solution (arr1, arr2)
   arr1.map.with_index { |n, i| (n - arr2[i]).abs ** 2 }.sum.fdiv(arr1.length)
 end
+
+# fdiv()將兩個數字相除後，結果為浮點數, num1為被除數, num2除數
+num1 = 5
+num2 = 2
+puts num1.fdiv(num2)
