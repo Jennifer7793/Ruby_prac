961cc5db2ca918c99924c0853f700c69fe833e7d
Jenjen <jennifer7793@gmail.com>
2022-07-05 13:45:12 +0800

    ruby variables such as number, boolean, string, symboal and float

---
+++ freeCodeCamp/ruby.rb
@@ -0,0 +1,19 @@
+# Reference - https://www.freecodecamp.org/news/learning-ruby-from-zero-to-hero-90ad4eecc82d?fbclid=IwAR13MOrTh3FcFqoz4PbGy-o1FLZLAwn99s5ARUCXvNJ1DN4mVa3zE8e7kk0
+
+# Variable. We can assign a value to whatever variable we want.
+one = 1
+two = 2
+some_number = 10000
+
+#booleans
+true_boolean = true
+false_boolean = false
+
+#string
+my_name = "Jennifer Lin"
+
+#symbol
+a_symbol = :my_symbol
+
+#float
+book_price = 15.80
