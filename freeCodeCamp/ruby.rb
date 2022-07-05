08fc3b5cfa5b1459bffc72cc6af5a1714bda1906
Jenjen <jennifer7793@gmail.com>
2022-07-05 13:57:56 +0800

    elsif and use if statment after the code to be executed

--- freeCodeCamp/ruby.rb
+++ freeCodeCamp/ruby.rb
@@ -32,3 +32,17 @@ if 1 > 2
 else
   puts "1 is not greater than 2"
 end
+
+if 1 > 2
+  puts "1 is greater than 2"
+elsif 2 > 1
+  puts "1 is not greater than 2"
+else 
+  puts "1 is equal to 2"
+end
+
+def hey_ho?
+  true
+end
+
+puts "let's go" if hey_ho?
