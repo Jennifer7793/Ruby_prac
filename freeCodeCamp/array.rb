8115bcecf158b2402d068df4cffe9560c6d396bd
Jenjen <jennifer7793@gmail.com>
2022-07-07 14:25:21 +0800

    mv class code to class.rb

--- freeCodeCamp/array.rb
+++ freeCodeCamp/array.rb
@@ -30,18 +30,3 @@ hash_jen["age"] = "4ever20"
 puts hash_jen
 
 hash_jen.each { |k, v| puts "#{k}: #{v}" }
-
-# "Class" is a way to define objects. 
-# "Objects" have two main characteristic: data and behavior. Vehicles have data like num of wheels and doors, behavior like accelerating and stopping
-# data = attributes; behavior = methods
-
-class Vehicle
-  def initialize(num_of_wheels, type_of_tank, seating_cap, max_velocity)
-    @num_of_wheels = num_of_wheels
-    @type_of_tank = type_of_tank
-    @seating_cap = seating_cap
-    @max_velocity = max_velocity
-  end
-end
-
-# use initialize method so when creating vehicle object, can define its attributes
---
+++ freeCodeCamp/class.rb
@@ -0,0 +1,14 @@
+# "Class" is a way to define objects. 
+# "Objects" have two main characteristic: data and behavior. Vehicles have data like num of wheels and doors, behavior like accelerating and stopping
+# data = attributes; behavior = methods
+
+class Vehicle
+  def initialize(num_of_wheels, type_of_tank, seating_cap, max_velocity)
+    @num_of_wheels = num_of_wheels
+    @type_of_tank = type_of_tank
+    @seating_cap = seating_cap
+    @max_velocity = max_velocity
+  end
+end
+
+# use initialize method so when creating vehicle object, can define its attributes
