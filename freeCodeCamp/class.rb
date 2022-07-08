90fcac9a2942e2c1ceb203439ba181d561dd67e5
Jenjen <jennifer7793@gmail.com>
2022-07-08 08:05:19 +0800

    attr_reader for getter

--- freeCodeCamp/class.rb
+++ freeCodeCamp/class.rb
@@ -3,6 +3,8 @@
 # data = attributes; behavior = methods
 
 class Vehicle
+  attr_reader :num_of_wheels
+
   def initialize(num_of_wheels, type_of_tank, seating_cap, max_velocity)
     @num_of_wheels = num_of_wheels
     @type_of_tank = type_of_tank
@@ -10,16 +12,17 @@ class Vehicle
     @max_velocity = max_velocity
   end
 
-  def num_of_wheels  #getter
-    @num_of_wheels
-  end
+  # def num_of_wheels  #getter
+  #   @num_of_wheels
+  # end
 
-  def set_num_of_wheels=(num)  #setter
-    @num_of_wheels = num
-  end
+  # def set_num_of_wheels=(num)  #setter
+  #   @num_of_wheels = num
+  # end
 end
 
 # use initialize method so when creating vehicle object, can define its attributes
 
 tesla_model_s = Vehicle.new(4, 'electric', 5, 250)
 p tesla_model_s
+p tesla_model_s.num_of_wheels
